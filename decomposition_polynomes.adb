c682ea75021250be019a73d23385caa7b6992d9e
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-03-25 18:51:38 +0100

    Fichier Main de base

---
+++ decomposition_polynomes.adb
@@ -0,0 +1,32 @@
+with Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
+use Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
+
+with Parseur, Defs, ABR, Svg;
+use Defs;
+
+procedure Decomposition_Polynomes is
+   Args_Invalides : exception;
+   Nb_Sommets : Natural;
+   T : TS_Ptr := null;
+begin
+   if Argument_Count /= 2 then
+      raise Args_Invalides;
+   end if;  
+   
+   Parseur.Lecture (Argument(1), Nb_Sommets, T);   
+   
+   Put_Line ("Nombre de sommets : " & Integer'Image(Nb_Sommets));
+   
+   for I in T.all'Range loop
+      Put (T.all(I).X);
+      Put (' ');
+      Put (T.all(I).Y);
+      New_Line;
+   end loop;
+   
+   Svg.Trace_Polygone (Argument(2), T);
+   
+exception
+   when Args_Invalides =>
+      Put_Line (Standard_Error, "utilisation : decpoly data.in out.svg");   
+end Decomposition_Polynomes;
