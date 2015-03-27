72e3a7d70219046095263c63eae189160575a6e1
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-03-27 20:34:50 +0100

    Initialisation des segments entrants sortants

--- decomposition_polygones.adb
+++ decomposition_polygones.adb
@@ -1,13 +1,36 @@
 with Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
 use Ada.Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
 
-with Parseur, Defs, ABR, Svg;
+with Parseur, Defs, ABR, Svg, Liste;
 use Defs;
 
 procedure Decomposition_Polygones is
    Args_Invalides : exception;
    Nb_Sommets : Natural;
-   T : TS_Ptr := null;
+   T : TSom_Ptr := null;
+   
+   procedure Init_Segments(T : TSom_Ptr) is
+   begin
+      for I in T.all'First..(T.all'Last - 1) loop	 
+	 if T(I).Pos.X < T(I + 1).Pos.X then
+	   Liste.Enqueue (T(I).Sortants, (T(I), T(I+1)));
+	 else
+	    Liste.Enqueue (T(I).Entrants, (T(I), T(I+1)));
+	 end if;
+      end loop;
+      
+      -- Prise en compte du segment entre le dernier et premier point
+      if T(T.all'Last).Pos.X < T(T.all'First).Pos.X then
+	 Liste.Enqueue (T(T.all'Last).Sortants, 
+			(T(T.all'Last), 
+			 T(T.all'First)));
+      else
+	 Liste.Enqueue (T(T.all'Last).Entrants, 
+			(T(T.all'Last), 
+			 T(T.all'First)));
+      end if;
+   end Init_Segments;
+   
 begin
    if Argument_Count /= 2 then
       raise Args_Invalides;
@@ -18,9 +41,9 @@ begin
    Put_Line ("Nombre de sommets : " & Integer'Image(Nb_Sommets));
    
    for I in T.all'Range loop
-      Put (T.all(I).X);
+      Put (T.all(I).Pos.X);
       Put (' ');
-      Put (T.all(I).Y);
+      Put (T.all(I).Pos.Y);
       New_Line;
    end loop;
    
