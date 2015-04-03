9416c6400a11cc4c5c1f5c8bc0946fe8da870e05
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-04-03 15:27:27 +0200

    Cleaner sag output

--- svg.adb
+++ svg.adb
@@ -1,25 +1,8 @@
 
 package body Svg is
-   
-   type Color is (Violet, Indigo, Bleu, Vert, Jaune, Orange, Rouge, Noir, Blanc);
-   function Code_Couleur (C : Color) return String is
-   begin
-      case C is
-         when Violet  => return "rgb(255,0,255)";
-         when Indigo  => return "rgb(111,0,255)";
-         when Bleu  => return "rgb(0,0,255)";
-         when Vert  => return "rgb(0,255,0)";
-         When Jaune => return "rgb(0,255,255)";
-         When Orange => return "rgb(255,165,0)";
-         when Rouge => return "rgb(255,0,0)";
-         when Noir  => return "rgb(0,0,0)";
-         when Blanc => return "rgb(255,255,255)";
-      end case;
-   end Code_Couleur;
-   
+      
    -- Dessine une ligne A -- B
-   procedure Svg_Line (A, B : in Position; 
-		       C: in Color)
+   procedure Svg_Line (A, B : in Position)
    is
    begin
       Put (Svg_Out, "<line x1=""");
@@ -30,10 +13,24 @@ package body Svg is
       Put (Svg_Out, B.X);
       Put (Svg_Out, """ y2=""");
       Put (Svg_Out, B.Y);
-      Put (Svg_Out, """ style=""stroke:");
-      Put (Svg_Out, Code_Couleur(C));
-      Put_Line (Svg_Out, ";stroke-width:0.1""/>");
+      Put (Svg_Out, """ style=""stroke:darkred");
+      Put_Line (Svg_Out, ";stroke-width:0.05"" transform=""translate(5,5)""/>");
    end Svg_Line;
+   
+   procedure Svg_Polygon (T : in TSom_Ptr)
+   is
+   begin
+      Put (Svg_Out, "<polygon points=""");
+      for I in T.all'Range loop
+	 Put (Svg_Out, T(I).Pos.X); 
+	 Put (Svg_Out, ',');
+	 Put (Svg_Out, T(I).Pos.Y);
+	 Put (Svg_Out, ' ');
+      end loop;
+      Put (Svg_Out, """ style=""fill:lightcyan;stroke:black;stroke-width:0.05""" 
+	     & " transform=""translate(5,5)"" />");
+   end Svg_Polygon;
+
    
    procedure Svg_Header is
       begin
@@ -60,11 +57,12 @@ package body Svg is
       Svg_Header;            
             
       -- Tracé du polygone
-      for I in T.all'First..(T.all'Last - 1) loop	 
-	 Svg_Line (T(I).Pos, T(I + 1).Pos, Bleu);
-      end loop;
+      --  for I in T.all'First..(T.all'Last - 1) loop	 
+      --  	 Svg_Line (T(I).Pos, T(I + 1).Pos, Bleu);
+      --  end loop;
+      Svg_Polygon (T);
       
-      Svg_Line (T(T.all'Last).Pos, T(T.all'First).Pos, Bleu);                  
+      --  Svg_Line (T(T.all'Last).Pos, T(T.all'First).Pos, Bleu);                  
    end Trace_Polygone;
    
    procedure Trace_Segments (Segs : in Liste_Segments) is
@@ -72,7 +70,7 @@ package body Svg is
    begin
       Seg_Cour := Segs.Tete;
       while Seg_Cour /= null loop
-	 Svg_Line (Seg_Cour.Seg.A.Pos, Seg_Cour.Seg.B.Pos, Vert);
+	 Svg_Line (Seg_Cour.Seg.A.Pos, Seg_Cour.Seg.B.Pos);
 	 Seg_Cour := Seg_Cour.Suiv;
       end loop;
       
