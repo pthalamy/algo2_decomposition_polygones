8e0b2a439e40cf0d8c8159c1cb70ac7aabc1c874
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-04-09 18:27:39 +0200

    Remove unnecessary MAJ_Voisinnage

--- Decpoly/generic_abr.adb
+++ Decpoly/generic_abr.adb
@@ -1,134 +1,115 @@
 package body Generic_ABR is
-   
-   -- Met à jour le compte de tous les noeuds Parents de "Parent", 
-   -- ce dernier y compris, de + X.
-   procedure MAJ_Voisinage (Parent : in out Arbre; X : in MAJ) is
-   begin
-      if Parent = null then 
-	 return;
-      else
-	 Parent.Compte := Parent.Compte + Integer(X);
-	 MAJ_Voisinage (Parent.Pere, X);
-      end if;
-   end MAJ_Voisinage;
-   
+
    -- Insere un noeud de valeur C dans l'arbre dont A est la racine
    -- Et retourne un pointeur sur son emplacement dans N
-   procedure Insertion (A : in out Arbre; 
+   procedure Insertion (A : in out Arbre;
 			C : in Type_Clef;
 			N : out Arbre) is
-   begin      
+   begin
       if A = null then
-	 A := new Noeud'(C => C, 
+	 A := new Noeud'(C => C,
 			 Fils => (others => null),
 			 Pere => null,
-			 Compte => 1);	 
+			 Compte => 1);
 	 N := A;
 	 return;
       end if;
-      
+
       if A.C > C then
+	 A.Compte := A.Compte + 1;
 	 if A.Fils(Gauche) = null then
-	    A.Fils(Gauche) := new Noeud'(C => C, 
+	    A.Fils(Gauche) := new Noeud'(C => C,
 					 Fils => (others => null),
 					 Pere => A,
 					 Compte => 1);
-	    MAJ_Voisinage (A, 1);
-	    
 	    N := A.Fils(Gauche);
-	 else 
+	 else
 	    Insertion (A.Fils(Gauche), C, N);
-	 end if;	 
+	 end if;
       else
+	 A.Compte := A.Compte + 1;
 	 if A.Fils(Droite) = null then
-	    A.Fils(Droite) := new Noeud'(C => C, 
+	    A.Fils(Droite) := new Noeud'(C => C,
 					 Fils => (others => null),
 					 Pere => A,
 					 Compte => 1);
-	    MAJ_Voisinage (A, 1);
-	    
+
 	    N := A.Fils(Droite);
-	 else 
+	 else
 	    Insertion (A.Fils(Droite), C, N);
 	 end if;
-      end if;      
-      
+      end if;
+
    end Insertion;
-   
+
    procedure Free is
       new Ada.Unchecked_Deallocation(Noeud, Arbre);
-   
-   -- Supprime un noeud de valeur C dans l'arbre dont A est la racine      
+
+   -- Supprime un noeud de valeur C dans l'arbre dont A est la racine
    procedure Suppression (A : in out Arbre; C : in Type_Clef) is
       Max : Type_Clef;
       Fils : Arbre;
-            
+
       -- Retourne la valeur la plus grande rencontrée dans le sous-arbre
       -- dont A est la racine
       procedure Sup_Max (A : in out Arbre; Max : out Type_Clef) is
-      begin 
-	 if A.Fils(Droite) = null then	    
+      begin
+	 if A.Fils(Droite) = null then
 	    Max := A.C;
 	    if A.Fils(Gauche) /= null then
 	       Fils := A.Fils(Gauche);
 	       Fils.Pere := A.Pere;
 	    end if;
-	    
-	    MAJ_Voisinage (A.Pere, -1);
-	    
+
 	    Free (A);
-	    
+
 	    A := Fils;
 	 else
-	    Sup_Max (A.Fils(Droite), Max);	    	    
+	    Sup_Max (A.Fils(Droite), Max);
 	 end if;
       end Sup_Max;
-      
-   begin      
+
+   begin
       if A = null then
 	 Put_Line (Standard_Error, "suppression_error: Valeur non présente");
 	 return;
       end if;
-      
+
       if A.C = C then
 	 if A.Fils(Gauche) = null and A.Fils(Droite) = null then
-	    if A.Pere /= null then	       	       
-	       MAJ_Voisinage (A.Pere, -1);
-	    end if;
-	    
-	    Free (A);	      
-	    A := null;	   
+
+	    Free (A);
+	    A := null;
 	 elsif A.Fils(Gauche) = null then
 	    Fils := A.Fils(Droite);
 	    Fils.Pere := A.Pere;
-	    Free (A);	       
+	    Free (A);
 	    A := Fils;
-	    
-	    MAJ_Voisinage (A.Pere, -1);
-	 elsif A.Fils(Droite) = null then	    
+
+	 elsif A.Fils(Droite) = null then
 	    Fils := A.Fils(Gauche);
 	    Fils.Pere := A.Pere;
-	    Free (A);	       
+	    Free (A);
 	    A := Fils;
-	    
-	    MAJ_Voisinage (A.Pere, -1);
 	 else
 	    Sup_Max (A.Fils(Gauche), Max);
 	    --  Put ("     Sup_Max = "); Put(Max); New_Line;
 	    A.C := Max;
-	 end if;	    
+	 end if;
       elsif A.C > C then
+	 A.Compte := A.Compte - 1;
 	 Suppression (A.Fils(Gauche), C);
       else -- A.C < C
-	 Suppression (A.Fils(Droite), C);		   
-      end if;      
+	 A.Compte := A.Compte - 1;
+	 Suppression (A.Fils(Droite), C);
+      end if;
    end Suppression;
-   
-   
+
+
    -- Recherche le noeud de valeur C dans l'arbre dont A est la racine
    -- Return True si trouvé, False sinon
    -- Retourne aussi R, pointeur sur le noeud recherché
-   procedure Recherche (A : in Arbre; 
+   procedure Recherche (A : in Arbre;
 			C : in Type_Clef;
 			R : out Arbre)  is
    begin
@@ -136,47 +117,47 @@ package body Generic_ABR is
 	 R := null;
 	 return;
       end if;
-      
+
       if A.C = C then
 	 R := A;
 	 return;
       elsif A.C > C then
-	 Recherche (A.Fils(Gauche), C, R);		   
+	 Recherche (A.Fils(Gauche), C, R);
       else
 	 Recherche (A.Fils(Droite), C, R);
       end if;
-   end Recherche;   
-   
+   end Recherche;
+
    -- Affiche le noeud N sur Stdout
    procedure Put (N : Noeud) is
    begin
       Put (N.C); Put (" (");
-      
-      --  Affichage du voisinnage       
+
+      --  Affichage du voisinnage
       if N.Pere /= null then
       	 Put (N.Pere.C); Put (", ");
       else
       	 Put ("       null, ");
       end if;
-      
+
       if N.Fils(Gauche) /= null then
       	 Put (N.Fils(Gauche).C); Put (", ");
       else
       	 Put ("       null, ");
       end if;
-      
+
       if N.Fils(Droite) /= null then
       	 Put (N.Fils(Droite).C); Put (")");
       else
       	 Put ("       null)");
       end if;
-      
-      --  Affichage de la profondeur du sous-arbre      
+
+      --  Affichage de la profondeur du sous-arbre
       Put (" || Depth : " & Integer'Image(N.Compte));
-      
+
       New_Line;
    end Put;
-   
+
    -- Affiche l'arbre dont A est la racine sur stdout
    procedure Affichage (A : in Arbre) is
    begin
@@ -184,58 +165,58 @@ package body Generic_ABR is
 	 Put_Line ("Arbre inexistant.");
 	 return;
       end if;
-      
-      if A.Fils(Gauche) /= null then	
+
+      if A.Fils(Gauche) /= null then
 	 Affichage (A.Fils(Gauche));
       end if;
-      
+
       if A.Fils(Droite) /= null then
 	 Affichage (A.Fils(Droite));
       end if;
-      
+
       Put (A.all);
    end Affichage;
-   
-   -- Affiche le code d'export au format dot du graph dont A est la racine 
+
+   -- Affiche le code d'export au format dot du graph dont A est la racine
    -- sur stdout
    procedure Export_Dot (A : in Arbre) is
-      
+
       procedure Export_Dot_Rec (SA : in Arbre) is
       begin
 	 if SA = null then
 	    return;
 	 end if;
-	 
+
 	 if SA.Fils(Gauche) /= null then
 	    Put (SA.C); Put (" -- "); Put (SA.Fils(Gauche).C);
 	    Export_Dot_Rec (SA.Fils(Gauche));
 	 end if;
-	 
+
 	 if SA.Fils(Droite) /= null then
 	    Put (SA.C); Put (" -- "); Put (SA.Fils(Droite).C);
 	    Export_Dot_Rec (SA.Fils(Droite));
 	 end if;
       end Export_Dot_Rec;
-      
+
    begin
       Put_Line ("=== Export .dot ===");
-            
+
       Put_Line ("graph mon_graphe {");
-      
+
       Export_Dot_Rec (A);
-      
-      Put_Line ("}");		  
+
+      Put_Line ("}");
    end Export_Dot;
-   
-   procedure Noeuds_Voisins (Cible : in Arbre; 
+
+   procedure Noeuds_Voisins (Cible : in Arbre;
 			     Petit_Voisin, Grand_Voisin : out Arbre) is
       Cour : Arbre;
    begin
       Petit_Voisin := null;
       Grand_Voisin := null;
-      
-      -- Grand_Voisin : 
-      
+
+      -- Grand_Voisin :
+
       if Cible.Fils(Droite) /= null then
 	 Cour := Cible.Fils(Droite);
 	 loop
@@ -251,9 +232,9 @@ package body Generic_ABR is
 	    Cour := Cour.Pere;
 	 end loop;
       end if;
-      
-      -- Petit_Voisin : 
-      
+
+      -- Petit_Voisin :
+
       if Cible.Fils(Gauche) /= null then
 	 Cour := Cible.Fils(Gauche);
 	 loop
@@ -269,55 +250,55 @@ package body Generic_ABR is
 	    Cour := Cour.Pere;
 	 end loop;
       end if;
-      
+
    end Noeuds_Voisins;
-   
+
    procedure Compte_Position (Cible : in Arbre;
 			      Nb_Petits : out Natural;
 			      Nb_Grands : out Natural) is
       Arbre_Courant : Arbre;
    begin
-      
+
       -- Initialisation des variables
       Nb_Petits := 0;
       Nb_Grands := 0;
       Arbre_Courant := Cible;
-      
-      if (Cible.all.Fils(Gauche) /= null) then 
+
+      if (Cible.all.Fils(Gauche) /= null) then
 	 -- On compte dans le sous-arbre Gauche
 	 Nb_Petits := Nb_Petits + Cible.all.Fils(Gauche).all.Compte;
       end if;
-      
+
       if (Cible.all.Fils(Droite) /= null) then
 	 -- On compte dans le sous-arbre droit
 	 Nb_Grands := Nb_Grands + Cible.all.Fils(Droite).all.Compte;
-      end if;  
-      
+      end if;
+
       while (Arbre_Courant.all.Pere /= null) loop -- On remonte
-	 
+
 	 if (Arbre_Courant.all.Pere.all.C > Arbre_Courant.all.C) then
 	    -- On différencie les cas où l'on est à droite ou à gauche du père
-	    
+
 	    Arbre_Courant := Arbre_Courant.all.Pere;
 	    Nb_Grands := Nb_Grands + 1;
-	    
-	    if (Arbre_Courant.all.Fils(Droite) /= null) then 
-	       Nb_Grands := Nb_Grands + 
+
+	    if (Arbre_Courant.all.Fils(Droite) /= null) then
+	       Nb_Grands := Nb_Grands +
 		 Arbre_Courant.all.Fils(Droite).all.Compte;
 	    end if;
-	    
+
 	 else -- (Arbre_Courant.all.Pere.all.C < Arbre_Courant.all.C) then
 	      -- On ne regarde pas le cas d'égalité
 	    Arbre_Courant := Arbre_Courant.all.Pere;
 	    Nb_Petits := Nb_Petits + 1;
-	    
+
 	    if (Arbre_Courant.all.Fils(Gauche) /= null) then
-	       Nb_Petits := Nb_Petits + 
+	       Nb_Petits := Nb_Petits +
 		 Arbre_Courant.all.Fils(Gauche).all.Compte;
 	    end if;
-	    
+
 	 end if;
       end loop;
-   end Compte_Position;   
-   
+   end Compte_Position;
+
 end Generic_ABR;
--- Decpoly/generic_abr.ads
+++ Decpoly/generic_abr.ads
@@ -7,54 +7,53 @@ generic
    type Type_Clef is private; -- nodevalue_t
    with function ">"(Left, Right : in Type_Clef) return Boolean is <>;
    with procedure Put (X : in Type_Clef) is <>; -- +Pratique pour DEBUG
-   
+
 package Generic_ABR is
-   
+
    type Noeud;
    type Arbre is access Noeud;
    type Direction is (Gauche , Droite);
-   type Tableau_Fils is array(Direction) of Arbre; 
-   type Noeud is record 
+   type Tableau_Fils is array(Direction) of Arbre;
+   type Noeud is record
       C : Type_Clef;
       Fils : Tableau_Fils;
       Pere : Arbre;
-      Compte : Positive; -- nombre de noeuds dans le sous−arbre 
+      Compte : Positive; -- nombre de noeuds dans le sous−arbre
    end record;
-   type MAJ is range -1 .. 1; -- Valeurs de mise à jour de l'attribut compte
-   
-   -- Insere un nouveau noeud de clef C dans l'arbre A et retourne un 
+
+   -- Insere un nouveau noeud de clef C dans l'arbre A et retourne un
    -- pointeur sur ce dernier dans N
    procedure Insertion (A : in out Arbre; C : in Type_Clef; N : out Arbre);
-   
+
    -- Supprime le noeud de clef C de l'arbre A
    procedure Suppression (A : in out Arbre; C : in Type_Clef);
-   
+
    -- Recherche le noeuf de clef  C dans l'arbre A
    -- Si ce dernier est trouvé : R = pointeur sur le noeud
    -- Sinon :                    R = null
-   procedure Recherche (A : in Arbre; 
+   procedure Recherche (A : in Arbre;
 			C : in Type_Clef;
 			R : out Arbre);
-   
+
    -- Affiche le contenu de l'arbre A dans le format suivant :
    -- NoeudCourant (Pere, FilsG, FilsD) || Compte : N
    procedure Affichage (A : in Arbre);
-   
+
    -- Exporte le contenu de l'arbre A dans un fichier .dot
    procedure Export_Dot (A : in Arbre);
-   
+
    -- Retourne les noeuds de clef inf et sup les plus proches du noeud Cible
-   procedure Noeuds_Voisins (Cible : in Arbre; 
+   procedure Noeuds_Voisins (Cible : in Arbre;
 			     Petit_Voisin, Grand_Voisin : out Arbre);
 
    -- Retourne le nombre de noeud de clef inf et sup à celle de Cible dans
    -- l'arbre auquel il appartient
-   procedure Compte_Position (Cible : in Arbre; 
-			      Nb_Petits : out Natural; 
+   procedure Compte_Position (Cible : in Arbre;
+			      Nb_Petits : out Natural;
 			      Nb_Grands : out Natural);
-   
-   
-private   
+
+
+private
    procedure Put (N : Noeud);
-   
+
 end Generic_ABR;
